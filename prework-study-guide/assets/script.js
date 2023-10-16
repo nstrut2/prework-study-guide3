84f8911852dedbb9153825da4e790c181763cd50
nstrut2 <strutzel.nick@gmail.com>
2023-10-16 11:18:40 -0500

    additional java work

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,12 +1,6 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-console.log('Here are the topics we learned through Prework:');
-
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
-
 function listTopics() {
     for (var x = 0; x < topics.length; x++) {
       console.log(topics[x]);
@@ -26,5 +20,7 @@ if (randomTopic === 'HTML') {
   console.log('Please try again!');
 }
 }
+console.log('Here are the topics we learned through Prework:');
+listTopics() 
 console.log('Which topic should we study first?');
-selectTopic();
+selectTopic()
